0311911429ca4a5ed7829192b753a2091f21fdfd
LVala <84684231+LVala@users.noreply.github.com>
2022-04-14 14:50:16 +0200

    Add parsing improvements to cw_05 zad1

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -13,6 +13,11 @@ struct Component {
     size_t length;
 };
 
+struct Executable {
+    char **cmds;
+    size_t length;
+};
+
 void count_lines(FILE *fp, int count[2]) {
     int c, i, was_n;
     count[0] = count[1] = i = was_n = 0;
@@ -51,19 +56,18 @@ int count_words(char *str) {
     return counted;
 }
 
-void parse_file(char *filepath, struct Component *comps, char **to_exec) {
+void parse_file(char *filepath, struct Component **comps, struct Executable **to_exec, int count[2]) {
+    // does not work, when command line argument includes whitespace, i.e. grep "sysopy fajne"
     FILE *fp = fopen(filepath, "r");
     if (fp == NULL) {
         fprintf(stderr, "File could be opened: %s\n", strerror(errno));
         exit(1);
     }
 
-    int count[2];
     count_lines(fp, count);
     fseek(fp, 0, SEEK_SET);
 
-    comps = calloc(count[0], sizeof(struct Component));
-    to_exec = calloc(count[1], sizeof(char*));
+    *comps = calloc(count[0], sizeof(struct Component));
 
     char *line = NULL;
     size_t len = 0;
@@ -74,40 +78,74 @@ void parse_file(char *filepath, struct Component *comps, char **to_exec) {
         char *saveptr = NULL;
         char *token = strtok_r(line, "=", &saveptr);
 
-        comps[i].name = calloc(strlen(token)+1, sizeof(char));
-        comps[i].cmds = calloc(MAX_SIZE, sizeof(char**));
-        comps[i].length = 0;
-        strcpy(comps[i].name, token);
+        (*comps)[i].name = calloc(strlen(token)+1, sizeof(char));
+        (*comps)[i].cmds = calloc(MAX_SIZE, sizeof(char**));
+        (*comps)[i].length = 0;
+        strcpy((*comps)[i].name, token);
 
-        while((token = strtok_r(NULL, "|", &saveptr)) && comps[i].length < MAX_SIZE) {
+        while((token = strtok_r(NULL, "|", &saveptr)) && (*comps)[i].length < MAX_SIZE) {
             char *temp = calloc(strlen(token)+1, sizeof(char));
             strcpy(temp, token);
             int words = count_words(temp);
-            comps[i].cmds[comps[i].length] = calloc(words+1, sizeof(char*));
-            comps[i].cmds[comps[i].length][words] = NULL;
+            (*comps)[i].cmds[(*comps)[i].length] = calloc(words+1, sizeof(char*));
+            (*comps)[i].cmds[(*comps)[i].length][words] = NULL;
 
             char *saveptr2 = NULL;
             char *subtoken = strtok_r(temp, " ", &saveptr2);
             int j = 0;
             while (subtoken) {
                 int word_len = strlen(subtoken);
-                comps[i].cmds[comps[i].length][j] = calloc(word_len+1, sizeof(char));
-                strcpy(comps[i].cmds[comps[i].length][j], subtoken);
+                (*comps)[i].cmds[(*comps)[i].length][j] = calloc(word_len+1, sizeof(char));
+                strcpy((*comps)[i].cmds[(*comps)[i].length][j], subtoken);
 
-                if (comps[i].cmds[comps[i].length][j][word_len-1] == '\n')
-                    comps[i].cmds[comps[i].length][j][word_len-1] = '\0';
+                if ((*comps)[i].cmds[(*comps)[i].length][j][word_len-1] == '\n')
+                    (*comps)[i].cmds[(*comps)[i].length][j][word_len-1] = '\0';
 
-                printf("%s\n", comps[i].cmds[comps[i].length][j]);
                 subtoken = strtok_r(NULL, " ", &saveptr2);
                 ++j;
             }
             free(temp);
-            ++comps[i].length;
+            ++(*comps)[i].length;
+        }
+        ++i;
+    }
+
+    *to_exec = calloc(count[1], sizeof(struct Component));
+    i = 0;
+    while (getline(&line, &len, fp) != -1) {
+        (*to_exec)[i].cmds = calloc(MAX_SIZE, sizeof(char*));
+        (*to_exec)[i].length = 0;
+        char *saveptr = NULL;
+        char *token = strtok_r(line, " | ", &saveptr);
+        while (token && (*to_exec)[i].length < MAX_SIZE) {
+            int word_len = strlen(token);
+            (*to_exec)[i].cmds[(*to_exec)[i].length] = calloc(strlen(token)+1, sizeof(char));
+            strcpy((*to_exec)[i].cmds[(*to_exec)[i].length], token);
+
+            if ((*to_exec)[i].cmds[(*to_exec)[i].length][word_len-1] == '\n')
+                    (*to_exec)[i].cmds[(*to_exec)[i].length][word_len-1] = '\0';
+
+            token = strtok_r(NULL, " | ", &saveptr);
+            ++(*to_exec)[i].length;
         }
         ++i;
     }
     free(line);
     fclose(fp);
+}
+
+void free_all(struct Component *comps, struct Executable *to_exec, int count[2]) {
+    for (int i=0; i<count[0]; ++i) {
+        for (int j=0; j < comps[i].length; ++j) {
+            for (int k=0; comps[i].cmds[j][k]; ++k) {
+                free(comps[i].cmds[j][k]);
+            }
+            free(comps[i].cmds[j]);
+        }
+        free(comps[i].cmds);
+        free(comps[i].name);
+    }
+    free(comps);
 }
 
 
@@ -118,9 +156,26 @@ int main(int argc, char *argv[]) {
     }
 
     struct Component *comps = NULL;
-    char **to_exec = NULL;
-    parse_file(argv[1], comps, to_exec);
+    struct Executable *to_exec = NULL;
+    int count[2];
 
-    // TODO freeing memory
+    parse_file(argv[1], &comps, &to_exec, count);
+
+    for (int i=0; i<count[0]; ++i) {
+        for (int j=0; j < comps[i].length; ++j) {
+            for (int k=0; comps[i].cmds[j][k]; ++k) {
+                printf("%s\n", comps[i].cmds[j][k]);
+            }
+        }
+    }
+
+    for (int i=0; i < count[1]; ++i) {
+        for (int j=0; j< to_exec[i].length; ++j) {
+            printf("%s\n", to_exec[i].cmds[j]);
+        }
+    }
+
+
+    free_all(comps, to_exec, count);
     return 0;
 }
